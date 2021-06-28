5410d738576e7e81eba34584f88135bef7ba1e55
Henrion Cedric <cedrichenrion5@gmail.com>
2021-06-28 19:53:05 +0200

    Put back the link for the JSON serialization when a request is made over the API

--- app/model/resource/BuildingResource.java
+++ app/model/resource/BuildingResource.java
@@ -46,6 +46,10 @@ public class BuildingResource {
         return id;
     }
 
+    public String getLink() {
+        return link;
+    }
+
     public String getName() {
         return name;
     }
